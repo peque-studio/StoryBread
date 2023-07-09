bc433af1df0bf6de13ed8190c320f46051d4c286
Mike Redko <30830926+somerandomdev49@users.noreply.github.com>
2023-07-09 18:17:21 +0300

    add prosemirror

--- src/pages/project/elements/contentEditor.ts
+++ src/pages/project/elements/contentEditor.ts
@@ -4,6 +4,37 @@ import createIcon from "../../../icons";
 import { BasicState, IReadonlyState, effectNow } from "../../../state";
 import { Api } from "../api/api";
 
+import { EditorState, Plugin } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import "prosemirror-view/style/prosemirror.css";
+import { Schema, DOMParser } from "prosemirror-model";
+import { schema as basicSchema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { exampleSetup } from "prosemirror-example-setup";
+import {
+  schema as mdSchema,
+  defaultMarkdownParser,
+  defaultMarkdownSerializer
+} from "prosemirror-markdown";
+import "prosemirror-example-setup/style/style.css";
+import "prosemirror-menu/style/menu.css";
+
+function createContentInput(node: Api.Node) {
+  const schema = mdSchema;
+  const editorEl = E("div.content-editor-editor");
+  const view = new EditorView(editorEl, {
+    state: EditorState.create({
+      doc: defaultMarkdownParser.parse(node.content.text.get())!,
+      plugins: exampleSetup({ schema }),
+    }),
+    dispatchTransaction(tr) {
+      view.updateState(view.state.apply(tr));
+      node.content.text.update(defaultMarkdownSerializer.serialize(view.state.doc));
+    },
+  });
+  return editorEl;
+}
+
 export default function createContentEditor(node: IReadonlyState<Api.Node | null>) {
   const state = new BasicState<HTMLElement | null>(null);
 	effectNow(node, (node) => {
@@ -26,7 +57,7 @@ export default function createContentEditor(node: IReadonlyState<Api.Node | null
 						}),
 					);
 				}),
-				E("textarea", (textareaEl) => bindToValue(textareaEl, node.content.text)),
+				createContentInput(node),
 			);
 		}));
 	});
