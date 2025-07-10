c6a2a2b1af37a4f7043533ec5971683d412ce97e
Kir Ivanov <kirivanov777@gmail.com>
2025-07-11 02:20:50 +0300

    feat: add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
-// import "./index.css";
+import "./index.css";
 
 import { routeTree } from "./routeTree.gen";
 
