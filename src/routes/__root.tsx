b4c2f11577c0d3273a368e126bae12e77b87d21c
Kir Ivanov <kirivanov777@gmail.com>
2025-07-11 02:47:53 +0300

    chore: dark mode only

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,12 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 });
--- src/routes/editor/-components/editor.tsx
+++ src/routes/editor/-components/editor.tsx
@@ -45,6 +45,7 @@ export function Editor() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         fitView
+        colorMode="dark"
       >
         <Controls />
         <MiniMap />
